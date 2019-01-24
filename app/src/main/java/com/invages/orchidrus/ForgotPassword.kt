5753d302a56870aa9d4f8ac329331cd01cba0f8b
MFCWL <ramachandrappa.udayakumar@mahindra.com>
2019-01-24 21:31:09 +0530

    Removed ActionBar and made plain full screen.

--- app/src/main/java/com/invages/orchidrus/ForgotPassword.kt
+++ app/src/main/java/com/invages/orchidrus/ForgotPassword.kt
@@ -9,13 +9,13 @@ class ForgotPassword : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        /*requestWindowFeature(Window.FEATURE_NO_TITLE)
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
-        )
+        )*/
         setContentView(R.layout.forgot_password)
 
-        actionBar?.hide()
+        supportActionBar?.hide()
     }
 }
--- app/src/main/java/com/invages/orchidrus/LoginPage.kt
+++ app/src/main/java/com/invages/orchidrus/LoginPage.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Window
 import android.view.WindowManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.login_page.*
 
@@ -11,17 +12,38 @@ class LoginPage : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        /*requestWindowFeature(Window.FEATURE_NO_TITLE)
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
-        )
+        )*/
         setContentView(R.layout.login_page)
 
-        actionBar?.hide()
+        supportActionBar?.hide()
+
+
+        passwordvisibility.setOnClickListener {
+
+        }
+
+        btLogin.setOnClickListener {
+
+            val mail = etEmail.text.toString().trim()
+            val pw = etPassword.text.toString().trim()
+
+            if (mail == "test" && pw == "123") {
+                Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show()
+            }else if(mail == ""){
+                Toast.makeText(this, "Please Enter mail id", Toast.LENGTH_SHORT).show()
+            } else if(pw == ""){
+                Toast.makeText(this, "Please Enter password", Toast.LENGTH_SHORT).show()
+            }
+        }
 
         forgot_password.setOnClickListener {
             startActivity(Intent(this, ForgotPassword::class.java))
         }
+
+
     }
 }
